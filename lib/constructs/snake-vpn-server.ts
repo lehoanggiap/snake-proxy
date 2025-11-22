8eeb670a879507d82a4fabca6ac5363e4ca8796c
giaplh <nhhgiap@gmail.com>
2025-11-22 23:02:14 +0700

    upgrade instance size

--- lib/constructs/snake-vpn-server.ts
+++ lib/constructs/snake-vpn-server.ts
@@ -110,7 +110,7 @@ export class SnakeVPNServer extends Construct {
       vpcSubnets: vpcSubnets || { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
       securityGroup: this.securityGroup,
 
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE9),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE4),
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
       }),
